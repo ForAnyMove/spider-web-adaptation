14bb86cb4e80f0ef9f26be97a4919643108c7681
ForAnyMove <alex.osadchuk.98@gmail.com>
2024-04-06 15:21:40 +0200

    story mode: pop-up adding

---
+++ src/StoryMode/storyMode.js
@@ -0,0 +1,19 @@
+// const levelButtons = document.getElementsByClassName('level-btn')
+const levelButtonsContainer = document.getElementsByClassName('story-map-container')[0]
+
+const closePopupButton = document.getElementById('close-popup');
+const popup = document.getElementById('popup');
+levelButtonsContainer.addEventListener('click', (event) => {
+  console.dir(event.target);
+  if (event.target.matches('.level-btn')) {
+    console.log('btn');
+    const levelBtn = event.target
+    console.log(popup.style.display);
+    popup.style.display = 'block'
+    // levelBtn.scrollIntoView({ behavior: 'smooth', block: 'center' });
+  }
+})
+  
+  closePopupButton.addEventListener('click', function() {
+      popup.style.display = 'none';
+  });  
