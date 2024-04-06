ca349e91abaa2e606877e1f3a087e87a720e1902
ForAnyMove <alex.osadchuk.98@gmail.com>
2024-04-06 16:56:49 +0200

    pop-up position

--- src/StoryMode/storyMode.js
+++ src/StoryMode/storyMode.js
@@ -6,10 +6,10 @@ const popup = document.getElementById('popup');
 levelButtonsContainer.addEventListener('click', (event) => {
   console.dir(event.target);
   if (event.target.matches('.level-btn')) {
-    console.log('btn');
+    // console.log('btn');
     const levelBtn = event.target
-    console.log(popup.style.display);
-    popup.style.display = 'block'
+    // console.log(popup.style.display);
+    popup.style.display = 'flex'
     // levelBtn.scrollIntoView({ behavior: 'smooth', block: 'center' });
   }
 })
@@ -17,3 +17,9 @@ levelButtonsContainer.addEventListener('click', (event) => {
   closePopupButton.addEventListener('click', function() {
       popup.style.display = 'none';
   });  
+
+  window.addEventListener('resize', () => {
+    console.log('resize');
+    popup.style.width = window.innerWidth;
+    popup.style.height = window.innerHeight
+  })
