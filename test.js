4e7c1e63cc5c5ffa464b92d9a41ff15e3abe6303
goldenitsir <golden.hack.non@gmail.com>
2024-04-26 22:38:37 +0300

    test challages

--- test.js
+++ test.js
@@ -5,15 +5,16 @@ openTestBtn.addEventListener('click', () => {
   console.log(testChallengeScreen);
   testChallengeScreen.style.display = 'flex'
   mainTab.style.display = 'none'
-  testAdd()
 })
-function testAdd () {
-  
-const container = document.getElementsByClassName('challenges-test-container')[0]
+function testAdd() {
+
+  const container = document.getElementsByClassName('challenges-test-container')[0]
   for (let i = 0; i < 15; i++) {
-    
-  const challengeTestCard = document.createElement('div')
-  challengeTestCard.classList.add('challenge-test-container')
-  container.appendChild(challengeTestCard)
+
+    const challengeTestCard = document.createElement('div')
+    challengeTestCard.classList.add('challenge-test-container')
+    container.appendChild(challengeTestCard)
   }
 }
+
+testAdd();
