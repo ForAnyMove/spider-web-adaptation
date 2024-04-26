651f2734eaf6d29b1c8bd94880f96ac962e1f61c
ForAnyMove <alex.osadchuk.98@gmail.com>
2024-04-26 21:03:04 +0200

    test slider

---
+++ test.js
@@ -0,0 +1,19 @@
+const openTestBtn = document.getElementById('challenge-test')
+const testChallengeScreen = document.getElementById('challenges-test-screen')
+const mainTab = document.getElementById('main-screen')
+openTestBtn.addEventListener('click', () => {
+  console.log(testChallengeScreen);
+  testChallengeScreen.style.display = 'flex'
+  mainTab.style.display = 'none'
+  testAdd()
+})
+function testAdd () {
+  
+const container = document.getElementsByClassName('challenges-test-container')[0]
+  for (let i = 0; i < 15; i++) {
+    
+  const challengeTestCard = document.createElement('div')
+  challengeTestCard.classList.add('challenge-test-container')
+  container.appendChild(challengeTestCard)
+  }
+}
